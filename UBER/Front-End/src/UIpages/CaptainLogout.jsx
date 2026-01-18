367a28185573fbaa3c36b8745501f8df1641d879
atul rathi <rathiatul43@gmal.com>
2026-01-18 21:51:08 +0530

    fix context error

--- UBER/Front-End/src/UIpages/CaptainLogout.jsx
+++ UBER/Front-End/src/UIpages/CaptainLogout.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect ,useContext} from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { CaptainDatacontext } from '../Components/logout';;
+import { CaptainDatacontext } from '../context/CaptainContext';;
 
 const Logout = () => {
   const navigate = useNavigate();
