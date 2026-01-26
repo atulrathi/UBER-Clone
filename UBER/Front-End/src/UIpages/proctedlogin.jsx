3a99119300121bd9390e09e0518a19c5318db10d
atul rathi <rathiatul43@gmal.com>
2026-01-26 20:15:23 +0530

    fix bug of login

--- UBER/Front-End/src/UIpages/proctedlogin.jsx
+++ UBER/Front-End/src/UIpages/proctedlogin.jsx
@@ -1,14 +1,18 @@
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
+const PublicRoute = ({ children }) => {
   const token = localStorage.getItem("ridertoken");
   const caption = localStorage.getItem("caption");
 
-  if (!token && !caption) {
-    return <Navigate to="/" replace />;
+  if (token) {
+    return <Navigate to="/home" replace />;
+  }
+
+  if (caption) {
+    return <Navigate to="/caption-home" replace />;
   }
 
   return children;
 };
 
-export default ProtectedRoute;
+export default PublicRoute;
